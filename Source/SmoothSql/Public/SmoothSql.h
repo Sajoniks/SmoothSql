2049ed58dbb3a3c04d174ed532e20baaf7b6ff78
Sajoniks <avesom33@mail.ru>
2021-12-01 17:44:33 +0300

    #FIX: Fixed crash related to printf

--- Source/SmoothSql/Public/SmoothSql.h
+++ Source/SmoothSql/Public/SmoothSql.h
@@ -32,20 +32,23 @@ DECLARE_LOG_CATEGORY_EXTERN(LogSmoothSqlite, Display, Display);
 		    \
 			void LogError(const TCHAR* Message, const FStringFormatOrderedArguments& Args)\
 			{\
+				FunctionFrame = FString(ANSI_TO_TCHAR(__FUNCTION__)).LeftChop(FString(L"::FSqliteTryCatchCtx::LogError").Len());\
 				SET_WARN_COLOR(COLOR_RED)\
-				GLog->Logf(ELogVerbosity::Error, L"[SmoothSql][Error] SQLite Reporting Exception <<< %s\nAt:%s, line %d", *FString::Format(Message, Args), *FunctionFrame, __LINE__);\
+				GLog->Logf(ELogVerbosity::Error, L"[SmoothSql][Error] SQLite Reporting Exception <<< %s\n\t>>> At:%s, line %d", *FString::Format(Message, Args), *FunctionFrame, __LINE__);\
 				CLEAR_WARN_COLOR()\
 			}\
 			void LogMsg(const TCHAR* Message, const FStringFormatOrderedArguments& Args)\
 			{\
+				FunctionFrame = FString(ANSI_TO_TCHAR(__FUNCTION__)).LeftChop(FString(L"::FSqliteTryCatchCtx::LogMsg").Len());\
 				SET_WARN_COLOR(COLOR_GREEN)\
-				GLog->Logf(ELogVerbosity::Display, L"[SmoothSql][Msg] SQLite Reporting <<< %s\nAt:%s, line %d", *FString::Format(Message, Args), *FunctionFrame, __LINE__);\
+				GLog->Logf(ELogVerbosity::Display, L"[SmoothSql][Msg] SQLite Reporting <<< %s\n\t>>> At:%s, line %d", *FString::Format(Message, Args), *FunctionFrame, __LINE__);\
 				CLEAR_WARN_COLOR()\
 			}\
 			void Log(const TCHAR* Intent)\
 			{\
+				FunctionFrame = FString(ANSI_TO_TCHAR(__FUNCTION__)).LeftChop(FString(L"::FSqliteTryCatchCtx::Log").Len());\
 				SET_WARN_COLOR(COLOR_RED)\
-				GLog->Logf(ELogVerbosity::Error, L"[SmoothSql][Error] SQLite Reporting Exception while %s\n<<< %s\nAt:%s, line %d\n<<< SQLite message: \"%s (%d)\"", Intent, *FunctionFrame, __LINE__, *ErrorMsg, ErrorCode);\
+				GLog->Logf(ELogVerbosity::Error, L"[SmoothSql][Error] SQLite Reporting Exception while %s\n\t<<< At:%s, line %d\n\t>>> SQLite message: \"%s (%d)\"", Intent, *FunctionFrame, __LINE__, *ErrorMsg, ErrorCode);\
 				CLEAR_WARN_COLOR()\
 			}\
 		} Ctx;\
@@ -54,8 +57,7 @@ DECLARE_LOG_CATEGORY_EXTERN(LogSmoothSqlite, Display, Display);
 #define SQLITE_CATCH \
 	} catch (SQLite::Exception& e) { \
 		Ctx.ErrorCode = e.getErrorCode(); \
-		Ctx.ErrorMsg = FString(UTF8_TO_TCHAR(e.getErrorStr()));\
-		Ctx.FunctionFrame = FString(ANSI_TO_TCHAR(__FUNCTION__));\
+		Ctx.ErrorMsg = FString(UTF8_TO_TCHAR(e.getErrorStr()));
 
 #define SQLITE_END \
 	} }
